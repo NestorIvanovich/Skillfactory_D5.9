278624cd809c1cb4368c2b65357eff3281c8d661
NestorIvanovich <115668331+NestorIvanovich@users.noreply.github.com>
2023-06-03 20:09:47 +0700

    Добавлен шаблон и вывод информации на странице

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
 from django.shortcuts import render
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    ordering = '-created_time'
+    template_name = 'news.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
